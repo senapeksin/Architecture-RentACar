997e709e87ffe78869299733484c46c4a3ea399c
Sena Pekşin <sena@endcozum.com>
2024-05-14 17:49:31 +0300

    commit

---
+++ Application/Features/Brands/Commands/CreateBrandCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using Application.Features.Brands.Commands.Create;
+using FluentValidation;
+
+namespace Application.Features.Brands.Commands;
+
+public class CreateBrandCommandValidator : AbstractValidator<CreateBrandCommand>
+{
+    public CreateBrandCommandValidator()
+    {
+        RuleFor(c => c.Name).NotEmpty().MinimumLength(2);
+    }
+}
