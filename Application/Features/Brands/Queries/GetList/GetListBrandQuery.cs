a63c6ae0e94b838cc7dc8e3b22390f9d9e81e6b9
Sena Pekşin <sena@endcozum.com>
2024-06-10 09:54:43 +0300

    added in memory cache

--- Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
+++ Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Caching;
 using Core.Application.Request;
 using Core.Application.Responses;
 using Core.Persistance.Paging;
@@ -8,9 +9,15 @@ using MediatR;
 
 namespace Application.Features.Brands.Queries.GetList;
 
-public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>
+public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>, ICachableRequest
 {
     public PageRequest PageRequest { get; set; }
+
+    public string CacheKey => $"GetListBrandQuery({PageRequest.PageIndex}, {PageRequest.PageSize})";
+
+    public bool ByPassCache { get; }
+
+    public TimeSpan? SlidingExpiration { get; }
 
     public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, GetListResponse<GetListBrandListItemDto>>
     {
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -11,6 +11,10 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 
+
+builder.Services.AddDistributedMemoryCache();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
