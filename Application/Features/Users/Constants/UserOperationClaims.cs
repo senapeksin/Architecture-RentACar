dcb4503db5b5decc9984439b1807b62c7fdbfaa0
Sena Pekşin <sena@endcozum.com>
2024-08-08 11:36:05 +0300

    Completed CreateUserCommand

---
+++ Application/Features/Users/Constants/UserOperationClaims.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Features.Users.Constants
+{
+    public static class UserOperationClaims
+    {
+        public const string Admin = "users.admin";
+
+        public const string Read = "users.read";
+        public const string Write = "users.write";
+
+        public const string Add = "users.add";
+        public const string Update = "users.update";
+        public const string Delete = "users.delete";
+    }
+}
